67a8dfc89d092fc8cc9f49b8fd338c35c1adb3af
Vicky <63042294+vickylskk@users.noreply.github.com>
2020-04-02 19:15:26 +0700

    First Commit
    connecting howcanpams into multiwii device

---
+++ HowCanPams/HCPsControls/HeadingIndicator.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Collections;
+using System.Drawing;
+using System.Text;
+using System.Data;
+
+namespace HowCanPamsControls
+{
+    class HeadingIndicator : HCPsGUIControl
+    {
+        #region Fields
+
+        float Heading;
+        bool ringType = true;
+
+        Bitmap ctr = new Bitmap(HowCanPams.HCPsControls.HCPsControlsResource.HeadingCenter);
+        Bitmap ring = new Bitmap(HowCanPams.HCPsControls.HCPsControlsResource.HeadingRing);
+        Bitmap ringM = new Bitmap(HowCanPams.HCPsControls.HCPsControlsResource.HeadingRingM);
+
+        #endregion
+
+        #region Constructor
+
+        private System.ComponentModel.Container components = null;
+
+        public HeadingIndicator()
+        {
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
+        }
+
+        #endregion
+
+        #region Component Designer generated code
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+        }
+        #endregion
+
+        #region Paint
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+
+            Pen maskPen = new Pen(this.BackColor, 30);
+            Bitmap bmp = new Bitmap(ring.Width, ring.Height);
+
+            Graphics gfx = Graphics.FromImage(bmp);
+            gfx.TranslateTransform(75, 75);   // pindahkan pusat putaran ke 75,75
+            gfx.RotateTransform(Heading);     // putarkan benda
+            gfx.TranslateTransform(-75, -75); // kembalikan pusat putaran ke titik 0,0
+            if (ringType) gfx.DrawImageUnscaled(ring, 0, 0);
+            else          gfx.DrawImageUnscaled(ringM, 0, 0);
+            pe.Graphics.DrawImageUnscaled(bmp, 0, 0);
+
+            pe.Graphics.DrawImageUnscaled(ctr, 0, 0);
+
+            System.Drawing.Font drawFont = new System.Drawing.Font(FontFamily.GenericMonospace, 9.0F);
+            System.Drawing.SolidBrush drawBrush = new System.Drawing.SolidBrush(System.Drawing.Color.White);
+            if (ringType) pe.Graphics.DrawString(String.Format("{0:000.00°}", Heading), drawFont, drawBrush, 50, 85);
+            else          pe.Graphics.DrawString(String.Format("{0:0000}", Math.Round(Heading*17.777)), drawFont, drawBrush, 59, 85);
+
+            gfx.Dispose();
+            bmp.Dispose();
+            maskPen.Dispose();
+            drawFont.Dispose();
+            drawBrush.Dispose();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Define the physical value to be displayed on the indicator
+        /// </summary>
+        /// <param name="aircraftHeading">The aircraft heading in °deg</param>
+        public void SetHeadingIndicatorParameters(float aircraftHeading, bool headingType)
+        {
+            if (aircraftHeading < 0) Heading = 360 + aircraftHeading;
+            else Heading = aircraftHeading;
+            ringType = headingType;
+            this.Refresh();
+        }
+
+        #endregion
+    }
+}
---
+++ HowCanPams/HCPsControls/PitchIndicator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Collections;
+using System.Drawing;
+using System.Text;
+using System.Data;
+
+namespace HowCanPamsControls
+{
+    class PitchIndicator : HCPsGUIControl
+    {
+        #region Fields
+
+        float Pitch;
+        float tgtPitch;
+
+        Bitmap holder = new Bitmap(HowCanPams.HCPsControls.HCPsControlsResource.PitchHolder);
+        Bitmap direct = new Bitmap(HowCanPams.HCPsControls.HCPsControlsResource.PitchDirect);
+        Bitmap target = new Bitmap(HowCanPams.HCPsControls.HCPsControlsResource.TargetDirect);
+
+        #endregion
+
+        #region Constructor
+
+        private System.ComponentModel.Container components = null;
+
+        public PitchIndicator()
+        {
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
+        }
+
+        #endregion
+
+        #region Component Designer generated code
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+        }
+        #endregion
+
+        #region Paint
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+
+            Pen maskPen = new Pen(this.BackColor, 30);
+
+            // display Ring
+            Bitmap bmp = new Bitmap(400, 330);
+            Graphics gfx = Graphics.FromImage(bmp);
+            gfx.TranslateTransform(103, 294);   //pindahkan pusat putaran ke 73,72
+            gfx.RotateTransform(-Pitch);    //putarkan benda
+            gfx.TranslateTransform(-103, -294); //kembalikan pusat putaran ke titik 0,0
+            gfx.DrawImageUnscaled(direct, 0, 0);
+            pe.Graphics.DrawImageUnscaled(bmp, 0, 0);
+
+            pe.Graphics.DrawImageUnscaled(holder, 0, 0);
+
+            // display Ring
+            Bitmap bmpTgt = new Bitmap(400, 330);
+            Graphics gfxTgt = Graphics.FromImage(bmpTgt);
+            gfxTgt.TranslateTransform(103, 294);   //pindahkan pusat putaran ke 73,72
+            gfxTgt.RotateTransform(-tgtPitch);    //putarkan benda
+            gfxTgt.TranslateTransform(-103, -294); //kembalikan pusat putaran ke titik 0,0
+            gfxTgt.DrawImageUnscaled(target, 0, 0);
+            pe.Graphics.DrawImageUnscaled(bmpTgt, 0, 0);
+
+            //System.Drawing.Font drawFont = new System.Drawing.Font(FontFamily.GenericMonospace, 9.0F);
+            //System.Drawing.SolidBrush drawBrush = new System.Drawing.SolidBrush(System.Drawing.Color.White);
+            //pe.Graphics.DrawString(String.Format("{0:000.00°}", Pitch), drawFont, drawBrush, 50, 85);
+
+            gfx.Dispose();
+            bmp.Dispose();
+            maskPen.Dispose();
+            //drawFont.Dispose();
+            //drawBrush.Dispose();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Define the physical value to be displayed on the indicator
+        /// </summary>
+        /// <param name="aircraftHeading">The aircraft heading in °deg</param>
+        public void SetPitchIndicatorParameters(float aircraftHeading, float TargetPitch)
+        {
+            if (aircraftHeading < 0) Pitch = 360 + aircraftHeading;
+            else Pitch = aircraftHeading;
+            if (TargetPitch < 0) tgtPitch = 360 + TargetPitch;
+            else tgtPitch = TargetPitch;
+            this.Refresh();
+        }
+
+        #endregion
+    }
+}
